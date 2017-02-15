77fc1f8a1d928eab0a4ce4def6aa4e170886c890
hasanagha <aghahasanraza@gmail.com>
2017-02-15 16:32:57 +0400

    default image added + js done for ajax search

--- static/js/spotify.js
+++ static/js/spotify.js
@@ -14,6 +14,8 @@
         'filter'        : '#search_type',
         'results'       : 'ul.results',
 
+        'default_image' : '/static/images/no_image.jpeg'
+
     };
 
     var TEMPLATE = '<li><img class="thumb" src="{image_url}">{label}</li>';
@@ -24,11 +26,18 @@
         {
             var _this = this;
 
+            $(SELECTORS.search_field).keypress(function (e) {
+                if (e.which == 13) {
+                    $(SELECTORS.search).click();
+                    return false;
+                }
+            });
+
             $('body').on("click", SELECTORS.search, function(){
                 var val = $(SELECTORS.search_field).val();
 
                 $(SELECTORS.loader).removeClass('hide-important');
-                $(this).hide();
+                $(SELECTORS.search).hide();
 
                 $.ajax({
                     url: CONFIG.search_url + $(SELECTORS.filter).val(),
@@ -43,30 +52,46 @@
                         _this.fillResults(response);
 
                         $(SELECTORS.loader).addClass('hide-important');
-                        $(this).show();
+                        $(SELECTORS.search).show();
                     }
                 });
             });
-            $(SELECTORS.title_field).keypress(function (e) {
-                if (e.which == 13) {
-                    $(SELECTORS.submitBtn).click();
-                    return false;
-                }
-            });
-            $(SELECTORS.submitBtn).click();
         },
-        fillResults: function(response){
+        fillResults: function(response) {
             var _this = this;
-            $('.counter').html(response.length + ' result(s) found').show();
-            if(response.length) {
-                var source   = $(SELECTORS.hb_item).html();
-                var template = Handlebars.compile(source);
-                var context = {presentations: response}
-                var html    = template(context);
+            var counter = 0;
+            var html = '';
+
+            if(response.status) {
+                var records = response.records[Object.keys(response.records)[0]];
+                counter = records.total;
+                $.each(records.items, function(){
+
+                    var image = SELECTORS.default_image;
+                    var images = this.images;
+
+                    if(images.length) {
+                        image = images.pop();
+                        image = image.url;
+                    }
+
+                    html += _this.createLi(this.name, image);                    
+                });
             } else {
-                var html = '';
+                html = 'error' in response?response.error:'No record found';
             }
+
+            $(SELECTORS.counter).html(counter + ' record(s)');
             $(SELECTORS.results).html(html);
+        },
+
+        createLi: function(label, image) {
+            var template = TEMPLATE;
+
+            var li = template.replace('{label}', label);
+            li = li.replace('{image_url}', image);
+
+            return li;
         }
     };
     $(document).ready(function(){
